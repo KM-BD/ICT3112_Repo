c3c65eaa57437d5f4bc100426b5bcc08f62a1ed1
karlo <39162639+KM-BD@users.noreply.github.com>
2024-09-27 11:12:45 +0800

    added testproject boolean in specflowtests

--- ICT3112_Calculator/SpecFlowCalculatorTests/obj/Debug/net8.0/SpecFlowCalculatorTests.AssemblyInfo.cs
+++ ICT3112_Calculator/SpecFlowCalculatorTests/obj/Debug/net8.0/SpecFlowCalculatorTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecFlowCalculatorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411b85b176050dcbc749d1bdad31dbe5fa849e60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd0961477bb9bc88647c2c8eb7dd08b9e5fa053")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecFlowCalculatorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpecFlowCalculatorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
